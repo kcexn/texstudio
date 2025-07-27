ec18eff9c9e6aec56bfca0c808d639745c23e8ba
Kevin Exton <kevin.exton@pm.me>
2025-07-27 12:13:06 +1000

    Revert "Searches and opens files from the additional input paths options. Can recursively search all subdirectories with a trailing // or \\, the same way that the tex configuration .ini files do it."
    This reverts commit 9f0c5d5283d3b5f02f20cdfcad9db276aa9a5042.

--- src/texstudio.cpp
+++ src/texstudio.cpp
@@ -9997,14 +9997,6 @@ void Texstudio::findMissingBracket()
 	if (c.isValid()) currentEditor()->setCursor(c);
 }
 
-static QString makeFilename(const QString &name, const QString &defaultExt) {
-    auto pos = name.lastIndexOf(".");
-    if (pos == 0) // names like "."
-        return name;
-    if (pos == -1 || name.back() == '.') // names without extension (can end with '.')
-        return name + defaultExt;
-    return name;
-}
 LatexEditorView* Texstudio::openExternalFile(QString name, const QString &defaultExt, LatexDocument *doc, bool relativeToCurrentDoc)
 {
 	if (!doc) {
@@ -10019,25 +10011,16 @@ LatexEditorView* Texstudio::openExternalFile(QString name, const QString &defaul
         name.chop(1);
     }
     QStringList curPaths;
-    curPaths << ensureTrailingDirSeparator(doc->getFileInfo().absolutePath());
     if (defaultExt == "bib") {
         curPaths << configManager.additionalBibPaths.split(getPathListSeparator());
     }
-    curPaths << configManager.additionalInputPaths.split(getPathListSeparator());
+    if(relativeToCurrentDoc){
+        curPaths<< ensureTrailingDirSeparator(doc->getFileInfo().absolutePath());
+    }
     LatexEditorView * loaded = nullptr;
-    const auto& fname = makeFilename(name, defaultExt);
-    auto cbegin = curPaths.cbegin(), cend = curPaths.cend();
-    for (auto cit=cbegin; !loaded && cit != cend; ++cit) {
-        using IteratorFlags = QDirIterator::IteratorFlags;
-        IteratorFlags flags{QDirIterator::NoIteratorFlags};
-        const auto& path = *cit;
-        // TeX configuration files treat trailing // or
-        // trailing \\ as a recursive glob.
-        if(path.endsWith("//") || path.endsWith("\\\\"))
-            flags |= QDirIterator::Subdirectories;
-        QDirIterator jt{path, {fname}, QDir::Files, flags};
-        while (!loaded && jt.hasNext())
-            loaded = load(QFileInfo(jt.next()).absoluteFilePath());
+    loaded = load(doc->getAbsoluteFilePath(name, defaultExt,curPaths));
+    if(loaded == nullptr){
+        loaded = load(doc->getAbsoluteFilePath(name, "",curPaths));
     }
 
     if (loaded == nullptr) {
